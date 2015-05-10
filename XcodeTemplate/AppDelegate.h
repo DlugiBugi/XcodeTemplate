1952e950e24f9c0d29876ec0106d8cb5f68995b3
Jakub Długosz <dlugosz.jakub@gmail.com>
2015-05-10 12:24:26 +0200

    Initalized Empty Project

---
+++ XcodeTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XcodeTemplate
+//
+//  Created by Jakub Dlugosz on 10.05.2015.
+//  Copyright (c) 2015 CodeSurf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XcodeTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeTemplate
+//
+//  Created by Jakub Dlugosz on 10.05.2015.
+//  Copyright (c) 2015 CodeSurf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
